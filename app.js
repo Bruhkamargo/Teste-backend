f0fc97219db5b371bb6b0088fbed1907c551a940
bruhkamargo <wwwbrunocamargo@hotmail.com>
2024-02-17 14:49:55 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001;
+
+const users = [
+    ["Nome", 18, true],
+    ["Nome", 11, false],
+    ["Nome", 46, true],
+]
+
+app.get('/', (req, res) => {
+    res.send('Hello Word!');
+});
+
+app.get('/users', (req, res) => {
+    res.send(users[0]);
+    console.log(req);
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
